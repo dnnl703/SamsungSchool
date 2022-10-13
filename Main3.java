57f3e9f626beeb1c7ce338b9390f830231b8b82a
onepunchman <102527189+dnnl703@users.noreply.github.com>
2022-10-13 11:11:55 +0300

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        String n1 =" TORTOV";
+        String n2 = " TORT";
+        String n3 =" TORTA";
+        if (s==0)
+            System.out.println(s+n1);
+        else if (s%100>=10 && s%100<=20)
+            System.out.println(s+n1);
+        else if (s%10==1)
+            System.out.println(s+n2);
+        else if (s%10>=2 && s%10<=4)
+            System.out.println(s+n3);
+        else
+            System.out.println(s + n1);
+
+    }
+}
---
+++ main4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == 0 && b <= 0 || a == 0 && b == 0 || a < 0 && b < 0 || b == 0 && a < 0)
+            System.out.println("no such x");
+        else if (b > 0 && a > 0 || a == 0 && b > 0 || b == 0 && a > 0)
+            System.out.println("any x");
+        if (a > 0 && b < 0) {
+            double n1 =(double) b/a;
+            double n2 = (double)-b/a;
+            String num1 = String.format("%.1f", n1);
+            String num2 = String.format("%.1f", n2);
+            System.out.println("x<" + num1 + " or x>" + num2);
+        } else if (a < 0 && b > 0) {
+            double n1 = (double) b/a;
+            double n2 = (double)-b/a;
+            String num1 = String.format("%.1f", n1);
+            String num2 = String.format("%.1f", n2);
+            System.out.println(num1 + "<x<" + num2);
+        }
+    }
+}
