4813eaef2f5b1e2d328dfc34786d7a5c370de9b7
onepunchman <102527189+dnnl703@users.noreply.github.com>
2022-10-17 21:06:26 +0300

    Update hw4.java

--- hw4.java
+++ hw4.java
@@ -3,20 +3,26 @@ package com.samsung;
 import java.util.Scanner;
 
 public class hw4 {
-
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int n = in.nextInt();
-
-        if (n % 100 > 9 && n % 100 < 20 || n % 10 > 4 || n == 0) {
-            System.out.println(n + " TORTOV");
-        }
-        else if (n % 10 == 1) {
-            System.out.println(n + " TORT");
-        }
-
-        else {
-            System.out.println(n + " TORTA");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a == 0 && b <= 0 || a == 0 && b == 0 || a < 0 && b < 0 || b == 0 && a < 0)
+            System.out.println("no such x");
+        else if (b > 0 && a > 0 || a == 0 && b > 0 || b == 0 && a > 0)
+            System.out.println("any x");
+        if (a > 0 && b < 0) {
+            double n1 =(double) b/a;
+            double n2 = (double)-b/a;
+            String num1 = String.format("%.1f", n1);
+            String num2 = String.format("%.1f", n2);
+            System.out.println("x<" + num1 + " or x>" + num2);
+        } else if (a < 0 && b > 0) {
+            double n1 = (double) b/a;
+            double n2 = (double)-b/a;
+            String num1 = String.format("%.1f", n1);
+            String num2 = String.format("%.1f", n2);
+            System.out.println(num1 + "<x<" + num2);
         }
     }
 }
